e65580a7358d00b78e7ef73dfe498bd059685b0b
don <donle22599@g.ucla.edu>
2019-02-06 15:40:12 -0800

    login API added

--- pondi/api.py
+++ pondi/api.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, permissions, generics
 from rest_framework.response import Response
+from .serializers import (CreateUserSerializer, UserSerializer, LoginUserSerializer)
 
 from knox.models import AuthToken
 
@@ -16,5 +17,25 @@ class RegistrationAPI(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)
         })
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)
+        })
+
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
 
 #The API is pretty staight-forward, we validate the user input and create an account if the validation passes. In the response, we return the user object in serialized format and an authentication token which will be used by the application to perform user-specific api calls.
