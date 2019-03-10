552ab5dd215a3a6ec9947d6452519ff76c161cc9
Don Le <donle22599@g.ucla.edu>
2019-03-10 13:42:41 -0700

    cors

--- pondiBackend/middlewares.py
+++ pondiBackend/middlewares.py
@@ -1,16 +1,4 @@
-class CorsFix(object):
+class CorsMiddleware(object):
     def process_response(self, req, resp):
         response["Access-Control-Allow-Origin"] = "*"
-        return response
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        response.setHeader("Access-Control-Allow-Headers",
-                           "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
         return response
